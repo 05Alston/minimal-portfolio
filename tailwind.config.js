9bba3038cba39b8ae53fadac3541d1a751f42c42
Alston Fernandes <89850018+05Alston@users.noreply.github.com>
2022-06-01 11:56:26 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    './*.{html,js}'
+  ],
+  theme: {
+    extend: {},
+    colors:{
+      'nord-0':'#2E3440',
+      'nord-1':'#3B4252',
+      'nord-2':'#434C5E',
+      'nord-3':'#ECEFF4',
+      'nord-4':'#8FBCBB',
+      'nord-5':'#81A1C1',
+      'nord-6':'#BF616A'
+    },
+    fontFamily: {
+      sans: ['Amiko', 'sans-serif'],
+      cursive: ['Grand Hotel', 'cursive'],
+    },
+  },
+  plugins: [],
+}
