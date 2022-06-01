4755e0bb55960429fff9d37a6cd1373368172229
Alston Fernandes <89850018+05Alston@users.noreply.github.com>
2022-06-01 19:50:02 +0530

    Added curves

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,25 @@ module.exports = {
     './*.{html,js}'
   ],
   theme: {
-    extend: {},
-    colors:{
+    extend: {
+      fontSize:{
+        '5.5xl':'3.2rem'
+      },
+      colors:{
       'nord-0':'#2E3440',
       'nord-1':'#3B4252',
       'nord-2':'#434C5E',
       'nord-3':'#ECEFF4',
       'nord-4':'#8FBCBB',
       'nord-5':'#81A1C1',
-      'nord-6':'#BF616A'
-    },
-    fontFamily: {
-      sans: ['Amiko', 'sans-serif'],
-      cursive: ['Grand Hotel', 'cursive'],
-    },
+      'nord-6':'#E8C888',
+      'transparent':'#00000000'
+      },
+      fontFamily: {
+        sans: ['Amiko', 'sans-serif'],
+        cursive: ['Grand Hotel', 'cursive'],
+      },
+    }
   },
   plugins: [],
 }
