488078d8133fd0817cbcd32271ed046bc4c0f039
Alston Fernandes <89850018+05Alston@users.noreply.github.com>
2022-06-06 21:28:01 +0530

    refactored stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,11 +15,21 @@ module.exports = {
       'nord-4':'#8FBCBB',
       'nord-5':'#81A1C1',
       'nord-6':'#E8C888',
+      'nord-7':'#BF616A',
       'transparent':'#00000000'
       },
       fontFamily: {
         sans: ['Amiko', 'sans-serif'],
         cursive: ['Grand Hotel', 'cursive'],
+      },
+      backgroundImage:{
+        'a':['url("footer.svg")']
+      },
+      maxWidth: {
+        '500': '500px',
+      },
+      height: {
+        '500': '500px',
       },
     }
   },
