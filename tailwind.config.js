8d523e617d2efa2f36d4318732b199742a6949c8
05Alston <0508alstonfernandes@gmail.com>
2022-07-04 16:21:17 +0530

    added projects

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,22 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        'sm': '400px',
+      },
       fontSize:{
         '5.5xl':'3.2rem'
       },
       colors:{
-      'nord-0':'#2E3440',
-      'nord-1':'#3B4252',
-      'nord-2':'#434C5E',
-      'nord-3':'#ECEFF4',
-      'nord-4':'#8FBCBB',
-      'nord-5':'#81A1C1',
-      'nord-6':'#E8C888',
-      'nord-7':'#BF616A',
+      'nord-0':'#2E3440',//black
+      'nord-1':'#3B4252',//black-gray
+      'nord-2':'#434C5E',//gray
+      'nord-3':'#ECEFF4',//white-gray
+      'nord-4':'#8FBCBB',//greenish
+      'nord-5':'#81A1C1',//blue
+      'nord-6':'#E8C888',//yellow
+      'nord-7':'#BF616A',//red
+      'nord-8':'#D08770',//orange
       'transparent':'#00000000'
       },
       fontFamily: {
@@ -23,13 +27,23 @@ module.exports = {
         cursive: ['Grand Hotel', 'cursive'],
       },
       backgroundImage:{
-        'a':['url("footer.svg")']
+        'curve1':['url("curve1.svg")'],
+        'curve2':['url("curve2.svg")'],
+        'curve3':['url("curve3.svg")'],
+        'curve4':['url("curve4.svg")'],
+        'footer':['url("footer.svg")'],
+        'card-gradient':['linear-gradient(#00000000 0%, #3B4252 90%, #2E3440 100%);'],
+        // 
+        'proj1':['url("me.jpg")'],
       },
       maxWidth: {
         '500': '500px',
       },
       height: {
         '500': '500px',
+      },
+      aspectRatio: {
+        '3/2': '3 / 2',
       },
     }
   },
